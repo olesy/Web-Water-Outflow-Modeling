45c73b2499611826b383c53464b81f660fd1da9a
the21composer <kirvant31580@gmail.com>
2021-05-04 17:05:24 +0300

    Неявный метод Эйлера чееек

--- js/engine.js
+++ js/engine.js
@@ -6,14 +6,14 @@ board = JXG.JSXGraph.initBoard('jxgbox-1', {
     axis:true,
     grid:true,
     showCopyright:false
-});
+})
 
 // Parameter sliders
-let R_slider = board.create('input', [-1, 10, '1.00', 'Input R: '], {cssStyle:'width: 100px'});
-let a_slider = board.create('input', [-1, 8, '0.70', 'Input a: '], {cssStyle:'width: 100px'});
-let h_slider = board.create('input', [-1, 6, '0.40', 'Input h: '], {cssStyle:'width: 100px'});
+let R_slider = board.create('input', [-1, 10, '1.00', 'Input R: '], {cssStyle:'width: 100px'})
+let a_slider = board.create('input', [-1, 8, '0.70', 'Input a: '], {cssStyle:'width: 100px'})
+let h_slider = board.create('input', [-1, 6, '0.40', 'Input h: '], {cssStyle:'width: 100px'})
 
-let methods = ["euler", "midpoint", "ab2", "ab3"]
+let methods = ["euler", "midpoint", "ab2", "ab3", "imp_euler"]
 
 let graphs = {
     "real":[null, null, true, "z(t)", {
@@ -33,12 +33,17 @@ let graphs = {
         "data":[],
         "abs_err":[],
         "err":[]
-    }, "purple", "AB2"],
+    }, "darkorange", "AB2"],
     "ab3":[null, null, true, "Метод Адамса-Башфорта 3", {
         "data":[],
         "abs_err":[],
         "err":[]
-    }, "olive", "AB3"]
+    }, "hotpink", "AB3"],
+    "imp_euler":[null, null, true, "Неявный метод Эйлера", {
+        "data":[],
+        "abs_err":[],
+        "err":[]
+    }, "saddlebrown", "IEU"]
 }
 
 // Change XY range
@@ -66,7 +71,7 @@ function update_graph(name, element) {
 
 // Write to HTML table methods points data, errors
 function update_table() {
-    let output = "<tr><th>t</th>";
+    let output = "<tr><th>t</th>"
     for (let elem in graphs) {
         if (graphs[elem][2]) {
             output += "<th>" + graphs[elem][3] + "</th>"
@@ -76,14 +81,14 @@ function update_table() {
     let errors = output
     let real = 0
     for (let i = 0; i < graphs.euler[4].data.length; i++) {
-        if (isNaN(graphs.midpoint[4].data[i][0])) break;
+        if (isNaN(graphs.midpoint[4].data[i][0])) break
         real = f_real(graphs.midpoint[4].data[i][1], parseFloat(R_slider.Value()), parseFloat(a_slider.Value()), startZ.Y())
         output += "<tr><td>" +
-            graphs.euler[4].data[i][1].toFixed(2) + "</td>";
+            graphs.euler[4].data[i][1].toFixed(2) + "</td>"
         errors += "<tr><td>" +
-            graphs.euler[4].data[i][1].toFixed(2) + "</td>";
+            graphs.euler[4].data[i][1].toFixed(2) + "</td>"
         abs_errors += "<tr><td>" +
-            graphs.euler[4].data[i][1].toFixed(2) + "</td>";
+            graphs.euler[4].data[i][1].toFixed(2) + "</td>"
         if (graphs.real[2]) {
             output += "</td><td>" + real.toString() + "</td>"
             errors += "</td><td>" + "0" + "</td>"
@@ -119,23 +124,23 @@ startZ = board.createElement('glider', [0, 10, board.defaultAxes.y], {
     name:'Height',
     strokeColor:'blue',
     fillColor:'blue'
-});
+})
 
 // Euler method function
 function solve_euler(x0, I, dt, f) {
-    let data = [x0];
-    let N = (I[1] - I[0]) / dt;
+    let data = [x0]
+    let N = (I[1] - I[0]) / dt
     for (let i = 1; i < N; ++i) {
-        let dx_dt = data[i - 1][0] + dt * f(0, data[i - 1])[0];
-        data.push([dx_dt]);
+        let dx_dt = data[i - 1][0] + dt * f(0, data[i - 1])[0]
+        data.push([dx_dt])
     }
     return data
 }
 
 // Adams-Bashforth 2 method function
 function solve_adams_bashforth_2(x0, I, dt, f) {
-    let data = [x0];
-    let N = (I[1] - I[0]) / dt;
+    let data = [x0]
+    let N = (I[1] - I[0]) / dt
     for (let i = 1; i < N; ++i) {
         if (i === 1) {
             // RK4 for 1st iteration
@@ -144,11 +149,11 @@ function solve_adams_bashforth_2(x0, I, dt, f) {
             let k3 = f(0, data[i - 1][0] + dt / 2 * k2)
             let k4 = f(0, data[i - 1][0] + dt * k3)
             let dx_dt = data[i - 1][0] + dt * (1 / 6 * k1 + 1 / 3 * k2 + 1 / 3 * k3 + 1 / 6 * k4)
-            data.push([dx_dt]);
+            data.push([dx_dt])
         } else {
             // Adams Bashforth, k=2
             let dx_dt = data[i - 1][0] + dt * (3 / 2 * f(0, data[i - 1][0]) - 1 / 2 * f(0, data[i - 2][0]))
-            data.push([dx_dt]);
+            data.push([dx_dt])
         }
     }
     return data
@@ -156,8 +161,8 @@ function solve_adams_bashforth_2(x0, I, dt, f) {
 
 // Adams-Bashforth 3 method function
 function solve_adams_bashforth_3(x0, I, dt, f) {
-    let data = [x0];
-    let N = (I[1] - I[0]) / dt;
+    let data = [x0]
+    let N = (I[1] - I[0]) / dt
     for (let i = 1; i < N; ++i) {
         if (i < 3) {
             // RK4 for 1st & 2nd iteration
@@ -166,12 +171,12 @@ function solve_adams_bashforth_3(x0, I, dt, f) {
             let k3 = f(0, data[i - 1][0] + dt / 2 * k2)
             let k4 = f(0, data[i - 1][0] + dt * k3)
             let dx_dt = data[i - 1][0] + dt * (1 / 6 * k1 + 1 / 3 * k2 + 1 / 3 * k3 + 1 / 6 * k4)
-            data.push([dx_dt]);
+            data.push([dx_dt])
         } else {
             // Adams Bashforth, k=3
             let dx_dt = data[i - 1][0] + dt * (23 / 12 * f(0, data[i - 1][0]) - 4 / 3 * f(0, data[i - 2][0])
                 + 5 / 12 * f(0, data[i - 3][0]))
-            data.push([dx_dt]);
+            data.push([dx_dt])
         }
     }
     return data
@@ -179,11 +184,11 @@ function solve_adams_bashforth_3(x0, I, dt, f) {
 
 // Midpoint method function
 function solve_midpoint(x0, I, dt, f) {
-    let data = [x0];
-    let N = (I[1] - I[0]) / dt;
+    let data = [x0]
+    let N = (I[1] - I[0]) / dt
     for (let i = 1; i < N; ++i) {
         let dx_dt = data[i - 1][0] + dt * f(0, data[i - 1][0] + dt / 2 * f(0, data[i - 1][0]))
-        data.push([dx_dt]);
+        data.push([dx_dt])
     }
     return data
 }
@@ -192,48 +197,89 @@ function solve_midpoint(x0, I, dt, f) {
 let f_real = function (t1, R, a, height) {
     let g = 9.80665
     let c1 = Math.sqrt(2 * height) / (R * R)
-    let y1 = [];
+    let y1 = []
     y1[0] = (Math.pow(a, 4) * g * Math.pow(t1, 2)) / (2 * Math.pow(R, 4)) + Math.pow(R, 4) * Math.pow(c1, 2) / 2 - Math.pow(a, 2) * Math.sqrt(g) * c1 * t1
-    return y1;
+    return y1
 }
 
 // Analytical solution function with step dt (for plot)
 function solve_real(x0, I, R, a, dt, height) {
-    let data = [x0];
+    let data = [x0]
     let g = 9.80665
     I[1] = Math.pow(R, 2) / Math.pow(a, 2) * Math.sqrt(2 * height / g)
     update_time(I[1])
-    let N = (I[1] - I[0]) / dt;
+    let N = (I[1] - I[0]) / dt
     for (let i = 1; i < N; i++) {
-        data.push([f_real(I[0] + dt * i, R, a, height)]);
+        data.push([f_real(I[0] + dt * i, R, a, height)])
+    }
+    return data
+}
+
+function solve_newton(x0, f, f_dot, tol = 1.0e-9) {
+    let max_iter = 50
+    let x = x0
+    let prev = 0
+    for (let i = 0; i < max_iter; i++) {
+        prev = x
+        x -= f(0, x) / f_dot(0, x)
+        if (Math.abs(x - prev) < tol) {
+            return x
+        }
+        if (isNaN(x)) {
+            return prev
+        }
+    }
+    return x
+}
+
+function solve_implicit_euler(x0, I, dt, f, f_dot) {
+    let data = [x0]
+    let N = (I[1] - I[0]) / dt
+    let exp_dot = function (t1, x1) {
+        return dt * f_dot(t1, x1) - 1
+    }
+    for (let i = 1; i < N; ++i) {
+        let exp = function (t1, x1) {
+            return (data[i - 1][0] + dt * f(t1, x1) - x1)
+        }
+        let dx_dt = solve_newton(data[i - 1][0], exp, exp_dot)
+        console.log(dx_dt)
+        data.push([dx_dt])
     }
     return data
 }
 
 // Universal ODE solver (any method)
 function ode_diff_water(method = "real", R, a, h, height, update) {
-    let I1 = [0, 13];
+    let I1 = [0, 13]
     if (h <= 0) h = 1
     if (a > R) a = R
     if (a <= 0) a = 1
     if (R <= 0) R = 1
     if (method === "real") {
-        h = 0.01;
+        h = 0.01
     }
     update(R, a, height)
 
     // xdot(t)
     let f1 = function (t1, x1) {
         let g = 9.80665
-        let y1 = [];
+        let y1 = []
         y1[0] = -(a * a) / (R * R) * Math.sqrt(2 * g * x1)
-        return y1;
-    };
+        return y1
+    }
 
-    let x01 = [height];
+    let f1_dot = function (t1, x1) {
+        let g = 9.80665
+        let y1 = []
+        y1[0] = -(a * a) / (2 * R * R * Math.sqrt(x1)) * Math.sqrt(2 * g)
+        return y1
+    }
+
+    let x01 = [height]
     let data1 = []
     if (method === "euler") {
-        data1 = solve_euler(x01, I1, h, f1);
+        data1 = solve_euler(x01, I1, h, f1)
     } else if (method === "midpoint") {
         data1 = solve_midpoint(x01, I1, h, f1)
     } else if (method === "ab2") {
@@ -242,20 +288,21 @@ function ode_diff_water(method = "real", R, a, h, height, update) {
         data1 = solve_adams_bashforth_3(x01, I1, h, f1)
     } else if (method === "real") {
         data1 = solve_real(x01, I1, R, a, h, height)
+    } else if (method === "imp_euler") {
+        data1 = solve_implicit_euler(x01, I1, h, f1, f1_dot)
     }
-
-    let q1 = I1[0];
+    let q1 = I1[0]
     for (let i = 0; i < data1.length; i++) {
-        data1[i].push(q1);
-        q1 += h;
+        data1[i].push(q1)
+        q1 += h
     }
-    return data1;
+    return data1
 }
 
 // Evaluating methods
-for (let i of ["euler", "midpoint", "ab2", "ab3", "real"]) {
+for (let i of ["euler", "midpoint", "ab2", "ab3", "imp_euler", "real"]) {
     graphs[i][4].data = ode_diff_water(i, parseFloat(R_slider.Value()), parseFloat(a_slider.Value()),
-        parseFloat(h_slider.Value()), startZ.Y(), update_parameters);
+        parseFloat(h_slider.Value()), startZ.Y(), update_parameters)
 }
 
 update_table()
@@ -266,24 +313,24 @@ for (let elem in graphs) {
     let data = []
     let t = []
     for (let i = 0; i < graphs[elem][4].data.length; i++) {
-        data[i] =graphs[elem][4].data[i][0];
-        t[i] =graphs[elem][4].data[i][1];
+        data[i] = graphs[elem][4].data[i][0]
+        t[i] = graphs[elem][4].data[i][1]
     }
-    graphs[elem][0] = board.create('curve', [t, data], {strokeColor:graphs[elem][5], strokeWidth:'3px'});
+    graphs[elem][0] = board.create('curve', [t, data], {strokeColor:graphs[elem][5], strokeWidth:'3px'})
     graphs[elem][0].updateDataArray = function () {
-        graphs[elem][4].data = ode_diff_water(i, parseFloat(R_slider.Value()), parseFloat(a_slider.Value()),
-            parseFloat(h_slider.Value()), startZ.Y(), update_parameters);
-        this.dataX = [];
-        this.dataY = [];
+        graphs[elem][4].data = ode_diff_water(elem, parseFloat(R_slider.Value()), parseFloat(a_slider.Value()),
+            parseFloat(h_slider.Value()), startZ.Y(), update_parameters)
+        this.dataX = []
+        this.dataY = []
         for (let i = 0; i < graphs[elem][4].data.length; i++) {
-            this.dataX[i] = graphs[elem][4].data[i][1];
-            this.dataY[i] = graphs[elem][4].data[i][0];
+            this.dataX[i] = graphs[elem][4].data[i][1]
+            this.dataY[i] = graphs[elem][4].data[i][0]
         }
-    };
+    }
     graphs[elem][1] = board.create('glider', [graphs[elem][0]],
         {
             name:graphs[elem][6],
             strokeColor:graphs[elem][5],
             fillColor:graphs[elem][5]
-        });
+        })
 }
